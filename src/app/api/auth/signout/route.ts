1b66ebf176397c5bdaf3bd08d4a98f1a9181cc31
ompateldeveloper <ompateldeveloper@gmail.com>
2025-01-08 18:42:44 +0530

    use search parram error resolved

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request) {
     const response = NextResponse.json({ message: "Signout successful" });
-    response.cookies.set("token", "", { expires: new Date(0) });
-    response.cookies.set("admin", "", { expires: new Date(0) });
+    response.cookies.set("token", "", { 
+        expires: new Date(0),
+        path: '/'
+    });
+    response.cookies.set("admin", "", { 
+        expires: new Date(0),
+        path: '/'
+    });
     return response;
 }
